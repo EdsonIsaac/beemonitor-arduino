446dc5c8abe702b7aa26784d8cdcfce90ddddcde
EdsonIsaac <edson-isaac@hotmail.com>
2021-11-25 20:08:53 -0300

    Adiciona mudanças para funcionamento no NodeMCU

--- monitoramento-colmeia-gsm.ino
+++ monitoramento-colmeia-gsm.ino
@@ -1,8 +1,9 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
 #include <DHT.h>
 #include <DHT_U.h>
 #include <HX711.h>
-#include <SoftwareSerial.h> 
-#include <String.h>
 
 #define DHTPIN 2
 #define DHTTYPE DHT11
@@ -12,10 +13,13 @@
 
 DHT dht(DHTPIN, DHT11);
 HX711 scale;
-SoftwareSerial gprsSerial(6, 7);
+HTTPClient http;
+WiFiClient client;
+
+const char* ssid = "yourNetworkName";
+const char* password = "yourNetworkPassword";
 
 const char codigo[] = "0001";
-const char telefone[] = "74988287606";
 
 long tempoDelay = 60000 * 1;
 unsigned long ultimaLeitura = 0;
@@ -25,14 +29,11 @@ float temperatura, umidade, peso;
 String url;
 
 void setup() {
-  gprsSerial.begin(9600);  
-  Serial.begin(9600);
   
-  dht.begin();
-  scale.begin(pinDT, pinSCK);
-  scale.set_scale(-422.50);
-  scale.tare();
- 
+  Serial.begin(115200);
+  setupWifi();
+  setupSensors();
+  
   delay(1000);
   sendData();
 }
@@ -59,81 +60,39 @@ void sendData() {
   
   delay(100);
 
-  if (gprsSerial.available()) Serial.write(gprsSerial.read());
- 
-  gprsSerial.println("AT");
-  delay(1000);
- 
-  gprsSerial.println("AT+CPIN?");
-  delay(1000);
- 
-  gprsSerial.println("AT+CREG?");
-  delay(1000);
- 
-  gprsSerial.println("AT+CGATT?");
-  delay(1000);
- 
-  gprsSerial.println("AT+CIPSHUT");
-  delay(1000);
- 
-  gprsSerial.println("AT+CIPSTATUS");
-  delay(2000);
- 
-  gprsSerial.println("AT+CIPMUX=0");
-  delay(2000);
- 
-  showSerialData();
- 
-  gprsSerial.println("AT+CSTT=\"gprs.oi.com.br\"");//start task and setting the APN,
-  delay(1000);
- 
-  showSerialData();
- 
-  gprsSerial.println("AT+CIICR");//bring up wireless connection
-  delay(3000);
- 
-  showSerialData();
- 
-  gprsSerial.println("AT+CIFSR");//get local IP adress
-  delay(2000);
- 
-  showSerialData();
- 
-  gprsSerial.println("AT+CIPSPRT=0");
-  delay(3000);
- 
-  showSerialData();
-  
-  gprsSerial.println("AT+CIPSTART=\"TCP\",\"185.28.23.22\",\"8081\"");//start up the connection
-  delay(6000);
- 
-  showSerialData();
- 
-  gprsSerial.println("AT+CIPSEND");//begin send data to remote server
-  delay(4000);
-  showSerialData();
-  
-  url="GET 185.28.23.22:8081/colmeias/update?codigo=" + String(codigo) + "&temperatura=" + String(temperatura) + "&umidade=" + String(umidade) + "&peso=" + String(peso) + "&telefone=" + String(telefone) + " HTTP/1.1\r\n\r\n";
-  Serial.println(url);
-  gprsSerial.println(url);//begin send data to remote server
-  
-  delay(4000);
-  showSerialData();
- 
-  gprsSerial.println((char)26);//sending
-  delay(5000);//waitting for reply, important! the time is base on the condition of internet 
-  gprsSerial.println();
-  
-  gprsSerial.println("AT+CIPSHUT");//close the connection
-  delay(100);
-  showSerialData();
+  if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
+    
+    http.begin(client, "https://beemonitor-backend.herokuapp.com/colmeias/update?codigo=" + String(codigo) + "&temperatura=" + String(temperatura) + "&umidade=" + String(umidade) + "&peso=" + String(peso));
+    int httpCode = http.GET(); //Send http request and return request status code
+
+    if (httpCode > 0) Serial.println(http.getString()); //Prints the request response
+    
+    http.end(); //
+  } else {
+      Serial.println("Erro! Conexão com a rede Wi-Fi não estabelecida!");
+      setupWifi();
+      sendData();
+  }
 }
 
-void showSerialData() {
+void setupSensors() {
+  dht.begin();
+  scale.begin(pinDT, pinSCK);
+  scale.set_scale(-422.50);
+  scale.tare();
+}
+
+void setupWifi () {
+  WiFi.begin(ssid, password);
   
-  while(gprsSerial.available() != 0) {
-    Serial.write(gprsSerial.read());
+  Serial.println("Conectando a rede Wi-Fi: " + (String)ssid);
+  
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
   }
-  
-  delay(5000);
+
+  Serial.println("");
+  Serial.print("Conexão realizada com sucesso! Endereço IP: ");
+  Serial.println(WiFi.localIP());
 }
