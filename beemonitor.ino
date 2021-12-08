431e71bbd52c2b49b97f4965a7721853b60b9ad8
EdsonIsaac <edson-isaac@hotmail.com>
2021-12-08 02:05:13 -0300

    Simplifica codigo para envio dos dados para o servidor

--- beemonitor.ino
+++ beemonitor.ino
@@ -17,7 +17,7 @@ SoftwareSerial gprsSerial(6, 7);
 const char codigo[] = "0001";
 const char telefone[] = "74988287606";
 
-long tempoDelay = 60000 * 1;
+long tempoDelay = 60000 * 15;
 unsigned long ultimaLeitura = 0;
 unsigned long tempoDecorrido = 0;
 
@@ -25,15 +25,17 @@ float temperatura, umidade, peso;
 String url;
 
 void setup() {
-  gprsSerial.begin(9600);  
   Serial.begin(9600);
+  
+  gprsSerial.listen();
+  gprsSerial.begin(9600);  
   
   dht.begin();
   scale.begin(pinDT, pinSCK);
   scale.set_scale(-422.50);
   scale.tare();
  
-  delay(1000);
+  delay(10000);
   sendData();
 }
  
@@ -51,89 +53,79 @@ void loop() {
   }
 }
 
+String getSerialData() {
+  return gprsSerial.available() != 0 ? gprsSerial.readString() : "";
+}
+/*
+void resendData() {
+  Serial.println("Erro! Falha ao enviar os dados! Será feita uma nova tentativa em alguns instantes!");
+
+  gprsSerial.println("AT+HTTPTERM");
+  delay(3000);
+  getSerialData();
+  
+  gprsSerial.println("AT+SAPBR=0,1");
+  delay(3000);
+  getSerialData();
+    
+  delay(10000);
+  sendData();
+}
+*/
 void sendData() {
   
   temperatura = dht.readTemperature(); 
   umidade = dht.readHumidity();
   peso = scale.get_units(5);
   
-  delay(100);
+  delay(1000);
 
-  if (gprsSerial.available()) Serial.write(gprsSerial.read());
- 
   gprsSerial.println("AT");
-  delay(1000);
- 
-  gprsSerial.println("AT+CPIN?");
-  delay(1000);
- 
-  gprsSerial.println("AT+CREG?");
-  delay(1000);
- 
-  gprsSerial.println("AT+CGATT?");
-  delay(1000);
- 
-  gprsSerial.println("AT+CIPSHUT");
-  delay(1000);
- 
-  gprsSerial.println("AT+CIPSTATUS");
-  delay(2000);
- 
-  gprsSerial.println("AT+CIPMUX=0");
-  delay(2000);
- 
-  showSerialData();
- 
-  gprsSerial.println("AT+CSTT=\"gprs.oi.com.br\"");//start task and setting the APN,
-  delay(1000);
- 
-  showSerialData();
- 
-  gprsSerial.println("AT+CIICR");//bring up wireless connection
   delay(3000);
- 
-  showSerialData();
- 
-  gprsSerial.println("AT+CIFSR");//get local IP adress
-  delay(2000);
- 
-  showSerialData();
- 
-  gprsSerial.println("AT+CIPSPRT=0");
-  delay(3000);
- 
-  showSerialData();
-  
-  gprsSerial.println("AT+CIPSTART=\"TCP\",\"185.28.23.22\",\"8081\"");//start up the connection
-  delay(6000);
- 
-  showSerialData();
- 
-  gprsSerial.println("AT+CIPSEND");//begin send data to remote server
-  delay(4000);
-  showSerialData();
-  
-  url="GET 185.28.23.22:8081/colmeias/update?codigo=" + String(codigo) + "&temperatura=" + String(temperatura) + "&umidade=" + String(umidade) + "&peso=" + String(peso) + "&telefone=" + String(telefone) + " HTTP/1.1\r\n\r\n";
-  Serial.println(url);
-  gprsSerial.println(url);//begin send data to remote server
-  
-  delay(4000);
-  showSerialData();
- 
-  gprsSerial.println((char)26);//sending
-  delay(5000);//waitting for reply, important! the time is base on the condition of internet 
-  gprsSerial.println();
-  
-  gprsSerial.println("AT+CIPSHUT");//close the connection
-  delay(100);
-  showSerialData();
-}
 
-void showSerialData() {
+  //if (getSerialData().indexOf("OK") == -1) resendData();
+
+  gprsSerial.println("AT+SAPBR=0,1");
+  delay(3000);
+
+  //if (getSerialData().indexOf("OK") == -1) resendData();
   
-  while(gprsSerial.available() != 0) {
-    Serial.write(gprsSerial.read());
-  }
+  gprsSerial.println("AT+SAPBR=3,1,\"APN\", \"gprs.oi.com.br\"");
+  delay(3000);
+
+  //if (getSerialData().indexOf("OK") == -1) resendData();
   
-  delay(5000);
+  gprsSerial.println("AT+SAPBR=1,1");
+  delay(3000);
+  
+  //if (getSerialData().indexOf("OK") == -1) resendData();
+
+  gprsSerial.println("AT+HTTPINIT");
+  delay(3000);
+
+  //if (getSerialData().indexOf("OK") == -1) resendData();
+
+  gprsSerial.println("AT+HTTPPARA=\"CID\",1");
+  delay(3000);
+
+  //if (getSerialData().indexOf("OK") == -1) resendData();
+
+  url="185.28.23.22:8081/colmeias/update?codigo=" + String(codigo) + "&temperatura=" + String(temperatura) + "&umidade=" + String(umidade) + "&peso=" + String(peso) + "&telefone=" + String(telefone);
+  Serial.println("Enviando dados para o servidor. URL: " + url);
+  gprsSerial.println("AT+HTTPPARA=\"URL\",\"" + url + "\"");
+  delay(3000);
+  
+  //if (getSerialData().indexOf("OK") == -1) resendData();
+
+  gprsSerial.println("AT+HTTPACTION=0");
+  delay(10000);
+  //getSerialData();
+  
+  gprsSerial.println("AT+HTTPTERM");
+  delay(3000);
+  //getSerialData();
+  
+  gprsSerial.println("AT+SAPBR=0,1");
+  delay(3000);
+  //getSerialData();
 }
